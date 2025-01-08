9f5ad436786c1520c9d834baab70ddd3fbc176af
Artha210102 <arthanugraha7945@gmail.com>
2025-01-09 03:01:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
-from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Set page title
 st.title("House Price Prediction using SVR")
